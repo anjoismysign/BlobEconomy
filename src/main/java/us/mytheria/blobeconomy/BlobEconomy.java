2794da1cd527ff390f997d17dcdc1ab9420f91d3
lbenav8095 <lubensaico@gmail.com>
2023-08-16 13:04:50 -0600

    Adds updater

--- src/main/java/us/mytheria/blobeconomy/BlobEconomy.java
+++ src/main/java/us/mytheria/blobeconomy/BlobEconomy.java
@@ -1,20 +1,30 @@
 package us.mytheria.blobeconomy;
 
+import org.jetbrains.annotations.NotNull;
 import us.mytheria.blobeconomy.director.EconomyManagerDirector;
+import us.mytheria.bloblib.entities.PluginUpdater;
 import us.mytheria.bloblib.managers.BlobPlugin;
 import us.mytheria.bloblib.managers.IManagerDirector;
 
 public final class BlobEconomy extends BlobPlugin {
     private IManagerDirector proxy;
+    private PluginUpdater updater;
 
     @Override
     public void onEnable() {
         EconomyManagerDirector director = new EconomyManagerDirector(this);
         proxy = director.proxy();
+        updater = generateGitHubUpdater("anjoismysign", "BlobEconomy");
     }
 
     @Override
     public IManagerDirector getManagerDirector() {
         return proxy;
+    }
+
+    @Override
+    @NotNull
+    public PluginUpdater getPluginUpdater() {
+        return updater;
     }
 }
