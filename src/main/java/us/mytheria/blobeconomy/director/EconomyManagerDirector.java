23a4bf2f53508ba339a3d08cbb3f1aac692ce0bd
lbenav8095 <lubensaico@gmail.com>
2024-06-07 08:03:55 -0600

    Adds German locale Adds Modern Greek locale Adds French locale Adds Dutch locale Adds Portuguese locale Adds Russian locale Adds Mandarin locale

--- src/main/java/us/mytheria/blobeconomy/director/EconomyManagerDirector.java
+++ src/main/java/us/mytheria/blobeconomy/director/EconomyManagerDirector.java
@@ -23,9 +23,43 @@ public class EconomyManagerDirector extends GenericManagerDirector<BlobEconomy> 
     public EconomyManagerDirector(BlobEconomy plugin) {
         super(plugin);
         getRealFileManager().unpackYamlFile("/currency", "default");
-        registerBlobMessage("es_es/blobeconomy_lang");
-        registerBlobInventory("Withdraw", "es_es/Withdraw");
-        registerBlobInventory("Trade", "es_es/Trade");
+        registerBlobMessage(
+                "es_es/blobeconomy_lang",
+                "de_de/blobeconomy_lang",
+                "el_gr/blobeconomy_lang",
+                "fr_fr/blobeconomy_lang",
+                "nl_nl/blobeconomy_lang",
+                "pt_pt/blobeconomy_lang",
+                "ru_ru/blobeconomy_lang",
+                "zh_cn/blobeconomy_lang"
+        );
+        registerTranslatableItem(
+                "es_es/blobeconomy_translatable_items",
+                "de_de/blobeconomy_translatable_items",
+                "el_gr/blobeconomy_translatable_items",
+                "fr_fr/blobeconomy_translatable_items",
+                "nl_nl/blobeconomy_translatable_items",
+                "pt_pt/blobeconomy_translatable_items",
+                "ru_ru/blobeconomy_translatable_items",
+                "zh_cn/blobeconomy_translatable_items");
+        registerBlobInventory("Withdraw",
+                "es_es/Withdraw",
+                "de_de/Withdraw",
+                "el_gr/Withdraw",
+                "fr_fr/Withdraw",
+                "nl_nl/Withdraw",
+                "pt_pt/Withdraw",
+                "ru_ru/Withdraw",
+                "zh_cn/Withdraw");
+        registerBlobInventory("Trade",
+                "es_es/Trade",
+                "de_de/Trade",
+                "el_gr/Trade",
+                "fr_fr/Trade",
+                "nl_nl/Trade",
+                "pt_pt/Trade",
+                "ru_ru/Trade",
+                "zh_cn/Trade");
         addManager("ConfigManager", new ConfigManager(this));
         addCurrencyDirector("Currency");
         addManager("TradeableDirector", new TradeableDirector(this));
